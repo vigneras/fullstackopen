eacd9bb292bef1922dfdab749b5c3ed48d193267
EC2 Default User <ec2-user@ip-172-31-41-249.us-west-2.compute.internal>
2022-06-18 01:18:07 +0000

    2.9: adding number

--- 2.6-phone-step1/src/App.js
+++ 2.6-phone-step1/src/App.js
@@ -3,37 +3,50 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '666777999' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event)
     setNewName(event.target.value)
   }
   
-  const addName =  (event) => {
+  const handleNumberChange = (event) => {
+    console.log(event)
+    setNewNumber(event.target.value)
+  }
+  
+  const addPerson =  (event) => {
     event.preventDefault()
-    console.log(event, persons, newName)
+    console.log(event, persons, newName, newNumber)
     if (persons.find(p => p.name === newName)) {
       alert(`Provided name ${newName} already exists in our db`)
       return
     }
     const personObject = {
       name: newName,
+      number: newNumber,
     }
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
                   value={newName}
                   onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input 
+                  value={newNumber}
+                  onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
--- 2.6-phone-step1/src/components/Person.js
+++ 2.6-phone-step1/src/components/Person.js
@@ -5,7 +5,7 @@ const Person = ({ person }) => {
   
   return (
     <div>
-    {person.name}
+    {person.name} {person.number}
     </div>
   )
 }
