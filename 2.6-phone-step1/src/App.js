1d22a6aca98ff10fe924ba454a6bd22ae793ad76
EC2 Default User <ec2-user@ip-172-31-41-249.us-west-2.compute.internal>
2022-06-18 01:36:12 +0000

    2.9 Filtering

--- 2.6-phone-step1/src/App.js
+++ 2.6-phone-step1/src/App.js
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '666777999' }
-  ]) 
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
 
   const handleNameChange = (event) => {
     console.log(event)
@@ -16,6 +21,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     console.log(event)
     setNewNumber(event.target.value)
+  }
+  
+  const handleFilterChange = (event) => {
+    console.log(event)
+    setNewFilter(event.target.value)
   }
   
   const addPerson =  (event) => {
@@ -37,6 +47,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter shown with: <input 
+                            value={newFilter}
+                            onChange={handleFilterChange} />
+      
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -54,7 +68,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person => <li> <Person person={person} /> </li>)}
+      {persons.filter(person => 
+                      person.name.toUpperCase().includes(newFilter.toUpperCase()))
+              .map(person => <li> <Person person={person} /> </li>)
+      }
       </ul>
     </div>
   )
