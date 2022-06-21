6ce37d756a8e6b5ac71773505beb7d0fd5b39b62
EC2 Default User <ec2-user@ip-172-31-41-249.us-west-2.compute.internal>
2022-06-21 04:35:27 +0000

    3.3 step 3 -- replies to /api/persons/id

--- 3.1-phonebook/index.js
+++ 3.1-phonebook/index.js
@@ -32,6 +32,18 @@ app.get('/api/info', (request, response) => {
   response.send(`<h1>Phonebook has info for ${persons.length} people</h1><br>${new Date()}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
