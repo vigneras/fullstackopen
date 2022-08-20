cdcd34121d1560d6ad226b07db4fb95cf5a07002
Pierre Vigneras <pierre.vigneras@gmail.com>
2022-08-20 19:42:36 +0000

    Add validation to user creation

--- bloglist-backend/controllers/users.js
+++ bloglist-backend/controllers/users.js
@@ -11,6 +11,10 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const {username, name, password} = request.body
 
+  if (!password || password.length < 3) {
+    return response.status(400).json({error: 'Password must be at least 3 characters long'})
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
--- bloglist-backend/tests/user_api.test.js
+++ bloglist-backend/tests/user_api.test.js
@@ -38,6 +38,70 @@ describe('when there is initially one user in db', () => {
     expect(usernames).toContain(newUser.username)
   })
 
+  test('creation failed without a username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      name: 'Pierre Vigneras',
+      password: 'sarengiv',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).not.toContain(newUser.username)
+  })
+
+  test('creation failed without a password', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'eipi',
+      name: 'Pierre Vigneras',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).not.toContain(newUser.username)
+  })
+
+  test('creation failed without a long enough password', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'eipi',
+      name: 'Pierre Vigneras',
+      password: 'ab',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).not.toContain(newUser.username)
+  })
+
   test('simple deletion succeeds', async () => {
     const usersAtStart = await helper.usersInDb()
     const userToDelete = usersAtStart[0]
